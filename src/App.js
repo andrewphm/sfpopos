427ba5322dd2765fd1cc8d771ae048c87408df06
Andrew Pham <andrew.pham@shopify.com>
2023-02-14 13:34:31 -0500

    Add public image assets and finish the Space component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Title from './components/Title';
+import POPOSList from './components/POPOSList';
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <POPOSList />
     </div>
   );
 }
---
+++ src/components/POPOSSpace.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function POPOSSpace({ name, image, address }) {
+  return (
+    <div>
+      <img src={`${process.env.PUBLIC_URL}/images/${image}`} width="300" height="300" alt="Hello" />
+      <h1>{name}</h1>
+      <div>{address}</div>
+    </div>
+  );
+}
+
+export default POPOSSpace;
