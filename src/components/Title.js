d428cf4d442689a052b186d607c271a038a477d5
Andrew Pham <andrew.pham@shopify.com>
2023-02-14 13:00:34 -0500

    Finish Title component

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './title.css';
+
+function Title() {
+  return (
+    <div className="title">
+      <h1>SFPOPOS</h1>
+      <div className="title-subtitle">San Franciscos Privately Owned Public Spaces</div>
+    </div>
+  );
+}
+
+export default Title;
