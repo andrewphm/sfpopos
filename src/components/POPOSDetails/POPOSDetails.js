df4cf94ae37d800ab0ee77d1fe7a6acb89f1addf
Andrew Pham <andrew.pham@shopify.com>
2023-02-16 13:03:51 -0500

    Add styles to POPOSDetails component

--- src/components/POPOSDetails/POPOSDetails.js
+++ src/components/POPOSDetails/POPOSDetails.js
@@ -9,17 +9,16 @@ function POPOSDetails(props) {
   const { images, title, desc, hours, features, geo } = data[id];
 
   return (
-    <div className="container">
-      <div>
-        <img src={`${process.env.PUBLIC_URL}images/${images[0]}`} alt={title} />
+    <div className="POPOSDetails">
+      <div className="POPOSDetails-image">
+        <img src={`${process.env.PUBLIC_URL}images/${images[0]}`} />
       </div>
-
-      <div>
-        <h1>{title}</h1>
-        <p>{desc}</p>
-        <p>{hours}</p>
-        <p>{features}</p>
-        <p>
+      <div className="POPOSDetails-info">
+        <h1 className="POPOSDetails-title">{title}</h1>
+        <p className="POPOSDetails-desc">{desc}</p>
+        <p className="POPOSDetails-hours">{hours}</p>
+        <p className="POPOSDetails-features">{features}</p>
+        <p className="POPOSDetails-geo">
           {geo.lat} {geo.lon}
         </p>
       </div>
